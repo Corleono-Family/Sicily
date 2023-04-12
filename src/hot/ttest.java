7824268e2094388f32e678c54880995cdb162f49
Admin <Admin@DESKTOP-NVVN2EB>
2023-04-12 14:55:16 +0800

    fddfh

--- src/hot/ttest.java
+++ src/hot/ttest.java
@@ -4,7 +4,7 @@ public class ttest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("gsdgsdgaga");
+		System.out.print("gsdgsyrrdgaga");
 
 	}
 
